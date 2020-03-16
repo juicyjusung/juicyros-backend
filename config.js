d5c07609a61235ac94e09ffc6e3ff0ea10b1ac86
juicyjusung <juicyjusung@gmail.com>
2020-03-16 23:05:49 +0900

    chore: 프로덕션 환경변수 설정 변경

--- config.js
+++ config.js
@@ -2,20 +2,10 @@
 import dotenv from 'dotenv';
 import fs from 'fs';
 
-// Set environment variables
-if (process.env.NODE_ENV === 'production') {
-  // Load the .env file from the system location where it is stored
-  const envConfig = dotenv.parse(fs.readFileSync('.env.production'));
-  for (let k in envConfig) {
-    process.env[k] = envConfig[k]
-  }
-  // for (const k in envConfig) {
-  //   process.env[k] = envConfig[k];
-  // }
-} else {
-  // Load the .env file from the system location where it is stored
+// Load the .env file from the system location where it is stored
+if (process.env.NODE_ENV === 'development') {
   const envConfig = dotenv.parse(fs.readFileSync('.env.development'));
   for (let k in envConfig) {
-    process.env[k] = envConfig[k]
+    process.env[k] = envConfig[k];
   }
 }
